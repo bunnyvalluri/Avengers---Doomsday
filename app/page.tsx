4df10773db1fe3ad0547d970d1d4c0df997f3add
Bunnyvalluri <vallurirahul3@gmail.com>
2026-08-20 07:32:59 +0530

    Fix React hydration mismatch by dynamically mounting Experience client component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import Experience from "@/components/Experience";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Experience = dynamic(() => import("@/components/Experience"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <Experience />;
